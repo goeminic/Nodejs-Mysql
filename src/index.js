63e4e21c187e416807349e15e831e3561fffbad6
goeminic <gnico2127@gmail.com>
2024-10-08 07:49:32 -0300

    configurando servidor

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //initialilzations
 const app = express();
 
 //settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views', 'partials')),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', 'hbs');
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 
 //Global Variables
+app.use((req, res, next) => {
+    next();
+});
 
 
 //Routes
 app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links',require('./routes/links'));
 
 //Public
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Starting Server
 app.listen(app.get('port'), () => {
@@ -24,4 +44,4 @@ app.listen(app.get('port'), () => {
 });
 
 
-//quede en 20
+//quede en 30
