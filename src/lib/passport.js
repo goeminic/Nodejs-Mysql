7d196da19fd819afe90781bd26df59f987e430f0
goeminic <gnico2127@gmail.com>
2024-11-25 07:27:10 -0300

    continua proceso de autenticacion

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -11,9 +11,20 @@ passport.use('local.signin', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, async(req, username, password, done) => {
-    console.log(req.body)
-    console.log(username)
-    console.log(password)
+
+    const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+    if(rows.length > 0){
+        const user = rows[0];
+        const validPassword = await helpers.matchPassword(password, user.password);
+        if(validPassword){
+            done(null, user, req.flash('Welcome ' + user.username));
+        } else {
+            done(null, false, req.flash('Contraseña Incorrecta'));
+        }
+    } else {
+        return done(null, false, req.flash('Usuario no Registrado'));
+    }
+
 }));
 
 
